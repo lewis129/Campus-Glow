bca28cf4df4a1e7e5fcadebf10389dd045158a64
lewis tech <lewisirungu489@gmail.com>
2026-01-29 14:51:11 +0300

    removed secret

--- campusGlow-Backend/firebase.js
+++ campusGlow-Backend/firebase.js
@@ -1,9 +1,14 @@
-const admin = require ("firebase-admin")
-const serviceAccount = require ("./serviceAccountKey.json")
+require("dotenv").config();
+const admin = require("firebase-admin");
 
 admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount),
-    storageBucket: "YOUR_PROJECT.appspot.com"
+  credential: admin.credential.cert({
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+    privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+  }),
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
 });
+
 const db = admin.firestore();
-module.exports = { db }
+module.exports = { db };
